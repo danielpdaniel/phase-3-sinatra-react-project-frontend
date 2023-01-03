f0cb57152f87403379481acf0ca525619b1f620c
danielpdaniel <artofjquinn@gmail.com>
2023-01-03 18:59:32 -0500

    uses fetch to /songs/:id to render song title, og artist, and map functioned cover artists

--- src/components/Song.js
+++ src/components/Song.js
@@ -4,28 +4,34 @@ import {useParams} from "react-router-dom";
 
 function Song() {
     const [song, setSong] = useState(false)
-    const [artist, setArtist] = useState(false)
+    const [artists, setArtists] = useState(false)
+    const [covers, setCovers] = useState(false)
 
     const params = useParams()
 
     useEffect(()=>{
     fetch(`http://localhost:9292/songs/${params.id}`)
     .then(r=>r.json())
-    .then(data=>setSong(data))}
+    .then(data=>{setSong(data)})}
     , [])
 
-    useEffect(()=>{
-        fetch(`http://localhost:9292/artists/${song.artist_id}`)
-        .then(r=>r.json())
-        .then(data=>setArtist(data.name))}
-        , [song])
+    // useEffect(()=>{
+    //     fetch(`http://localhost:9292/songs/${params.id}/covers`)
+    //     .then(r=>r.json())
+    //     .then(data=>setCovers(data))
+    // }, [])
+
     return (
         <div>
             <h2>SONG !</h2>
             {song ? 
             <div>
-            <h3>{song.title}</h3>
-            <h4>By {artist}</h4> 
+                <h3>{song.title}</h3>
+                <h4>By {song.artist.name}</h4> 
+                <h4>Covers:</h4>
+                <ul>
+                    {song.covers.map(cover => <li key={cover.id}>{cover.artist.name}</li>)}
+                </ul>
             </div>
             : <h3>Loading...</h3>}
         </div>
