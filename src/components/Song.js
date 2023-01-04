73394f5d0b5966a717ce2d9364c11f52a7fa5e56
danielpdaniel <artofjquinn@gmail.com>
2023-01-04 17:45:36 -0500

    sets up a search/dropdown feature for the available artists in the new cover form

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,20 +3,63 @@ import React, { useEffect, useState } from "react";
 import {useParams} from "react-router-dom";
 
 function Song({ artists }) {
-    const [song, setSong] = useState(false)
-    const [formStatus, setFormStatus] = useState(false)
-    const [artistSearchTerm, setArtistSearchTerm] = useState(false)
+    const [song, setSong] = useState(false);
+    const [covers, setCovers] = useState(false);
+    const [formStatus, setFormStatus] = useState(false);
+    const [artistSearchTerm, setArtistSearchTerm] = useState("");
+    const [filteredArtists, setFilteredArtists] = useState(false);
+    const [formArtist, setFormArtist] = useState(false);
+    const [formPerformanceLink, setFormPerformanceLink] = useState(false);
 
     const params = useParams()
 
     useEffect(()=>{
     fetch(`http://localhost:9292/songs/${params.id}`)
     .then(r=>r.json())
-    .then(data=>{setSong(data)})}
+    .then(data=>{setSong(data); setCovers(data.covers)})}
     , [])
 
     function handleNewCoverClick(){
         setFormStatus(formStatus => !formStatus)
+    }
+
+    function handleArtistSearch(e){
+        if (e.target.value === ""){
+            setArtistSearchTerm("")
+            setFilteredArtists(false)
+        }else{
+       const searchValue = e.target.value
+        setArtistSearchTerm(searchValue)
+        setFilteredArtists(true)
+        }
+    }
+
+    function handleArtistClick(artist) {
+        setFormArtist(artist)
+        setArtistSearchTerm(artist.name)
+        setFilteredArtists(false)
+    }
+
+    function handleInputChange(e) {
+        setFormPerformanceLink(e.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+       const postBody = {
+            song_id: song.id,
+            artist_id: formArtist.id,
+            performance_link: formPerformanceLink
+        }
+        fetch(`http://localhost:9292/covers`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postBody)
+        })
+        .then(r=>r.json())
+        .then(data=>{setSong([...song, data]); console.log(data)})
     }
 
     return (
@@ -31,22 +74,27 @@ function Song({ artists }) {
                 <button onClick={handleNewCoverClick}>Add a Cover of {song.title}!</button>
                 {formStatus ? 
                 <div>
-                    <form>
-                        <label for="artist">Artist Name</label>
+                    <form onSubmit={handleSubmit}>
+                        <label>Artist Name</label>
                         <br></br>
                         {/* <select>
                             {artists.map(artist => <option key={artist.id} value={artist.name}>{artist.name}</option>)}
                         </select> */}
-                        <input type="text" name="artist"/>
+                        <input type="text" name="artist" onChange={handleArtistSearch} value={artistSearchTerm}/>
+                        <div className="dropdown">
+                        {filteredArtists ? artists.map(artist => artist.name.toUpperCase().includes(artistSearchTerm.toUpperCase()) ? <p key={artist.id} onClick={()=>handleArtistClick(artist)}>{artist.name}</p> : null) : null}
+
+                        </div>
                         <br></br>
-                        <label for="performance_link">YouTube link</label>
+                        <label>YouTube link</label>
                         <br></br>
-                        <input type="text" name="performance_link"/>
+                        <input type="text" name="performance_link" onChange={handleInputChange}/>
+                        <input type="submit"/>
                     </form>
                 </div>
                 : null}
                 </div>
-                    {song.covers.map(cover => 
+                    {covers.map(cover => 
                     <div key={cover.id}>
                         <h5>
                         {cover.artist.name}
