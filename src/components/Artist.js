ef6697e7ba58a4dbf21c2397ae7c73239dcb6d71
danielpdaniel <artofjquinn@gmail.com>
2023-01-06 14:42:30 -0500

    fixes Artist component to conditionally render specific artist obj

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Artist({artists}) {
     const params = useParams()
 
-    const artist = artists ? artists.filter(artist => artist.id === 3) : false
+    const artist = artists ? artists.filter(artist => artist.id === parseInt(params.id, 10))[0] : false;
+    
     // console.log(artist)
     return(
         <div>
-            <h2>{artist ? artist[0].name : "Loading..."}</h2>
+            <h2>{artist ? artist.name : "Loading..."}</h2>
         </div>
     )
 }
