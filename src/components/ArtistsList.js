9949cdb6d1638f6bc55f148936155168255f78f1
danielpdaniel <artofjquinn@gmail.com>
2023-01-08 16:10:26 -0500

    conditionally renders state controlled artist edit input

--- src/components/ArtistsList.js
+++ src/components/ArtistsList.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import CreateArtist from './CreateArtist';
 
 function ArtistsList({ artists, onArtistsChange }) {
+    const [editStatus, setEditStatus] = useState(null);
+    const [artistNameEdit, setArtistNameEdit] = useState(null)
+    console.log(editStatus)
     
+    function handleEditClick(artist){
+        editStatus !== artist.id ? setEditStatus(artist.id) : setEditStatus(null);
+        artistNameEdit !== artist.name ? setArtistNameEdit(artist.name) : setArtistNameEdit(null);
+    }
+
+    function handleEditInput(e) {
+        setArtistNameEdit(e.target.value)
+    }
     return (
         <div>
             <CreateArtist artists={artists} onArtistsChange={onArtistsChange}/>
         <h2>Artists!</h2>
-        <ul>
+        {/* <ul> */}
         {artists ? artists.map(artist =>
-        <li key={artist.id}>
-            <NavLink to={`/artists/${artist.id}`}>
-            {artist.name}
-            </NavLink>
-        </li>)
+            <li key={artist.id}>
+                {artist.id === editStatus ?
+                // <form>
+                    <input placeholder={artist.name} value={artistNameEdit} onChange={handleEditInput}/>
+                // </form>
+                    :
+                <NavLink to={`/artists/${artist.id}`}>
+                {artist.name}
+                </NavLink>}
+                <button onClick={()=>handleEditClick(artist)}>{artist.id === editStatus ? "Cancel" : "Edit"}</button>
+            </li>)
              : <h4>Loading...</h4>}
-        </ul>
+        {/* </ul> */}
         </div>
     )
 }
