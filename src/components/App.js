82b6af7e5b9320a1db11ac5c761e0596809e148e
danielpdaniel <artofjquinn@gmail.com>
2023-01-24 13:03:33 -0500

    removes unused functions/commented out code from App

--- src/components/App.js
+++ src/components/App.js
@@ -65,30 +65,6 @@ function App() {
     setSongs(updatedSongs)
   }
 
-  // function handleArtistUpdate(artistData, deleteStatus){
-  //   console.log(artistData)
-  //   if(!deleteStatus){
-  //   const objToEdit = artists.filter(artist => artist.id === artistData.id)[0]
-  //   const updatedArtists = objToEdit ? artists.map(artist => artist.id !== artistData.id ? artist : artistData) : [...artists, artistData]
-  //   setArtists(updatedArtists)
-  //   }else {
-  //     const updatedArtists = artists.filter(artist => artist.id !== artistData.id)
-    
-  //     setArtists(updatedArtists)
-      
-  //     const updatedSongs = songs.filter(song => song.artist_id !== artistData.id)
-      
-  //     const updatedSongsDeletedArtistsCoversRemoved = updatedSongs.map(song => {
-  //       const theseCovers = song.covers.filter(cover => cover.artist_id !== artistData.id)
-  //       const thisSong = song
-  //       thisSong.covers = theseCovers
-  //       return thisSong
-  //     })
-      
-  //     setSongs(updatedSongsDeletedArtistsCoversRemoved)
-  //   }
-  // }
-
   function handleNewSong(songData){
     const updatedSongs = [...songs, {...songData,
     covers: []}]
@@ -141,18 +117,6 @@ function App() {
     setSongs(updatedSongs)
     setArtists(updatedArtists)
   }
-
-  // function handleSongUpdate(songData){
-  //   console.log(songData)
-  //   const updatedArtists = artists.map(artist => artist.id !== songData.artist.id ? artist : songData.artist)
-
-  //   const songObj = songs.find(song => song.id === songData.id)
-  //   const updatedSongs = songObj ? songs.map(song => song.id !== songData.id ? song : songData) : [...songs, songData]
- 
-  //   setArtists(updatedArtists)
-  //   setSongs(updatedSongs)
-
-  // }
 
   function handleNewCover(coverData){
     const updatedSongs = songs.map(song => {
@@ -268,24 +232,6 @@ function App() {
     setSongs(updatedSongs)
     setArtists(updatedArtists)
   }
-
-  function handleCoverUpdate(coverData){
-    const updatedArtists = artists.map(artist => artist.id !== coverData.song.artist.id ? artist : coverData.song.artist)
-    const updatedCoverArtist = updatedArtists.map(artist => artist.id !== coverData.artist_id ? artist : coverData.artist)
-    setArtists(updatedCoverArtist)
-
-    const updatedSongs = songs.map(song => song.id !== coverData.song_id ? song : coverData.song)
-    setSongs(updatedSongs)
-  }
-
-    // function handleFetchDataInState(data){
-  //   const updatedArtists = artists.map (artist => artist.id !== data.artist.id ? artist : data.artist)
-  //   const updatedSongs = []
-  //   updatedArtists.forEach(artist => Boolean(artist.songs) ? artist.songs.forEach(song => updatedSongs.push(song)) : null)
-
-  //   setArtists(updatedArtists);
-  //   setSongs(updatedSongs);
-  // }
 
   return (
     <div>
